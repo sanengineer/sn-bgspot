3737838553793b0d3ab88ec58587458eba5f8748
sanengineer <48153104+sanengineer@users.noreply.github.com>
2020-12-27 23:42:32 +0700

    update code

--- sanfinite.js
+++ sanfinite.js
@@ -1,9 +1,9 @@
-(function($) {
-var loadingGif = 'https://lh3.googleusercontent.com/-FiCzyOK4Mew/T4aAj2uVJKI/AAAAAAAAPaY/x23tjGIH7ls/s32/ajax-loader.gif';
-var olderPostsLink = '';
+var loadingGif =
+  "https://lh3.googleusercontent.com/-FiCzyOK4Mew/T4aAj2uVJKI/AAAAAAAAPaY/x23tjGIH7ls/s32/ajax-loader.gif";
+var olderPostsLink = "";
 var loadMoreDiv = null;
-var postContainerSelector = 'div.blog-posts';
-var loading =  false;
+var postContainerSelector = "div.blog-posts";
+var loading = false;
 
 var win = $(window);
 var doc = $(document);
@@ -11,11 +11,9 @@ var doc = $(document);
 var rscript = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
 
 function loadDisqusScript(domain) {
-  $.getScript('http://' + domain + '.disqus.com/blogger_index.js');
+  $.getScript("http://" + domain + ".disqus.com/blogger_index.js");
 }
 
-}
-  
 function loadMore() {
   if (loading) {
     return;
@@ -27,13 +25,13 @@ function loadMore() {
     return;
   }
 
-  loadMoreDiv.find('a').hide();
-  loadMoreDiv.find('img').show();
+  loadMoreDiv.find("a").hide();
+  loadMoreDiv.find("img").show();
   $.ajax(olderPostsLink, {
-    'dataType': 'html'
-  }).done(function(html) {
-    var newDom = $('<div></div>').append(html.replace(rscript, ''));
-    var newLink = newDom.find('a.blog-pager-older-link');
+    dataType: "html",
+  }).done(function (html) {
+    var newDom = $("<div></div>").append(html.replace(rscript, ""));
+    var newLink = newDom.find("a.blog-pager-older-link");
 
     var newPosts = newDom.find(postContainerSelector).children();
     $(postContainerSelector).append(newPosts);
@@ -41,7 +39,7 @@ function loadMore() {
     // Loaded more posts successfully.  Register this pageview with
     // Google Analytics.
     if (window._gaq) {
-      window._gaq.push(['_trackPageview', olderPostsLink]);
+      window._gaq.push(["_trackPageview", olderPostsLink]);
     }
     // Render +1 buttons.
     if (window.gapi && window.gapi.plusone && window.gapi.plusone.go) {
@@ -51,7 +49,7 @@ function loadMore() {
     if (window.disqus_shortname) {
       loadDisqusScript(window.disqus_shortname);
     }
-    
+
     // Render Facebook buttons.
     if (window.FB && window.FB.XFBML && window.FB.XFBML.parse) {
       window.FB.XFBML.parse();
@@ -62,13 +60,13 @@ function loadMore() {
     }
 
     if (newLink) {
-      olderPostsLink = newLink.attr('href');
+      olderPostsLink = newLink.attr("href");
     } else {
-      olderPostsLink = '';
+      olderPostsLink = "";
       loadMoreDiv.hide();
     }
-    loadMoreDiv.find('img').hide();
-    loadMoreDiv.find('a').show();
+    loadMoreDiv.find("img").hide();
+    loadMoreDiv.find("a").show();
 
     loading = false;
   });
@@ -76,9 +74,10 @@ function loadMore() {
 
 function getDocumentHeight() {
   return Math.max(
-      win.height(),
-      doc.height(),
-      document.documentElement.clientHeight);
+    win.height(),
+    doc.height(),
+    document.documentElement.clientHeight
+  );
 }
 
 function handleScroll() {
@@ -90,11 +89,11 @@ function handleScroll() {
 }
 
 function init() {
-  if (_WidgetManager._GetAllData().blog.pageType == 'item') {
+  if (_WidgetManager._GetAllData().blog.pageType == "item") {
     return;
   }
 
-  olderPostsLink = $('a.blog-pager-older-link').attr('href');
+  olderPostsLink = $("a.blog-pager-older-link").attr("href");
   if (!olderPostsLink) {
     return;
   }
@@ -108,10 +107,8 @@ function init() {
   loadMoreDiv = $('<div style="text-align: center; font-size: 150%;"></div>');
   loadMoreDiv.append(link);
   loadMoreDiv.append(img);
-  loadMoreDiv.insertBefore($('#blog-pager'));
-  $('#blog-pager').hide();
+  loadMoreDiv.insertBefore($("#blog-pager"));
+  $("#blog-pager").hide();
 }
 
 $(document).ready(init);
-
-})(jQuery);
